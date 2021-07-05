144e7c2cedd1315fde50a615bfc8e0a974219e4d
shamalipathirana <shyamalee.123.pathirana@gmail.com>
2021-07-05 16:23:51 +0530

    static dir to serve static content

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ const port = process.env.PORT || 3000
 
 const publicStaticDirPath = path.join(__dirname, '../public');
 
+// static dir to serve static content
+app.use(express.static(publicStaticDirPath));
+
 app.listen(port, () => {
     console.log("Server is up and running on port: " + port);
 });
