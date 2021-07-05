8ebdaf4afd530c746475ae8cd6ab5b3114f40d29
shamalipathirana <shyamalee.123.pathirana@gmail.com>
2021-07-05 18:29:52 +0530

    exports module

--- utils/weatherData.js
+++ utils/weatherData.js
@@ -1,8 +1,10 @@
 const request=require('request');
 const constants=require('../config');
 
-const weathreData=[address,callback] => {
+const weatherData = (address,callback) => {
     const url=constants.openWeatherMap.BASE_URL + encodeURIComponent(address) + '&appid=' + constants.openWeatherMap.SECRET_KEY;
     console.log(url);
     callback(true);
 }
+
+module.exports = weatherData;
