c4642956b750a5707709aae28ef1a10631b8e9cd
shamalipathirana <shyamalee.123.pathirana@gmail.com>
2021-07-05 22:16:56 +0530

    cjange weatherData.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const hbs = require('hbs');
 const path = require('path');
 const app = express();
 
+const weatherData = require('../utils/weatherData');
+
 const port = process.env.PORT || 3000
 
 const publicStaticDirPath = path.join(__dirname, '../public');
@@ -10,7 +12,6 @@ const publicStaticDirPath = path.join(__dirname, '../public');
 const viewsPath = path.join(__dirname, '../templates/views');
 
 const partialsPath = path.join(__dirname, '../templates/partials');
-
 // setup handlebars engine and templates path
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
@@ -19,6 +20,21 @@ hbs.registerPartials(partialsPath);
 // static dir to serve static content
 app.use(express.static(publicStaticDirPath));
 
-app.listen(port, () => {
-    console.log("Server is up and running on port: " + port);
+app.get('', (req, res) => {
+    res.send("This is my weather app")
 });
+
+
+
+// localhost:3000/products?address=lahore
+app.get('/weather', (req, res) => {
+     res.send("This is weather end point");
+});
+// 404 route should come at last
+app.get("*", (req, res) => {
+    res.send("Page not found");
+});
+
+app.listen(port, () => {
+    console.log("Server is up and running on port: " , port);
+})
--- utils/weatherData.js
+++ utils/weatherData.js
@@ -1,10 +1,20 @@
-const request=require('request');
+const request =require('request');
 const constants=require('../config');
 
 const weatherData = (address,callback) => {
     const url=constants.openWeatherMap.BASE_URL + encodeURIComponent(address) + '&appid=' + constants.openWeatherMap.SECRET_KEY;
     console.log(url);
-    callback(true);
+    request({url,json:true},(error,{body} ) =>{
+        console.log(body);
+        if(error){
+            callback("can not fetch data from open weather map api", undefined)
+        }
+        else{
+            callback(undefined,{
+                abc:true
+            })
+        }
+    })
 }
 
 module.exports = weatherData;
