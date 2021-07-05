5626fe994fccaae1bb89c8f9eb4a02dc72f75808
shamalipathirana <shyamalee.123.pathirana@gmail.com>
2021-07-05 18:25:50 +0530

    update weatherData.js file

---
+++ utils/weatherData.js
@@ -0,0 +1,8 @@
+const request=require('request');
+const constants=require('../config');
+
+const weathreData=[address,callback] => {
+    const url=constants.openWeatherMap.BASE_URL + encodeURIComponent(address) + '&appid=' + constants.openWeatherMap.SECRET_KEY;
+    console.log(url);
+    callback(true);
+}
