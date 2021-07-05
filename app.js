16be431d85059f6ac2e9d4e5b79975d6c0b2fb63
shamalipathirana <shyamalee.123.pathirana@gmail.com>
2021-07-05 23:17:53 +0530

    create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+const axios = require('axios');
+
+axios.get('/user?ID=12345')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  });
+  
