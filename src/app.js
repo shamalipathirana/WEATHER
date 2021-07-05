ff305e58ed5d5faaabbc0db15a28fca1b5f1ddbd
shamalipathirana <shyamalee.123.pathirana@gmail.com>
2021-07-05 16:20:25 +0530

    add css and js folder to public folder

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const hbs = require('hbs');
+const path = require('path');
 const app = express();
 
 const port = process.env.PORT || 3000
+
+const publicStaticDirPath = path.join(__dirname, '../public');
 
 app.listen(port, () => {
     console.log("Server is up and running on port: " + port);
