9b83f9cf714b06d91995906ea75a16338bc6fd35
shamalipathirana <shyamalee.123.pathirana@gmail.com>
2021-07-05 18:04:40 +0530

    create config.js file

---
+++ utils/config.js
@@ -0,0 +1,9 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL:'https://api.openweathermap.org/data/2.5/weather?q=',
+        SECRECT_KEY:'1c87c3e486588c6bc96c779d879eec75',
+        
+    } 
+    
+}
+
